d46943c2bbd15658811bfec8ce49bdeec71316c2
Conor <63920243+Conjons7@users.noreply.github.com>
2020-06-08 14:46:02 -0700

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import AddTodo from './AddTodo';
+import TodoList from './TodoList';
 class App extends Component {
 
   render() {
@@ -9,42 +10,10 @@ class App extends Component {
         <p style={{color: "white"}}>Track all of the things</p>
         <hr />
         <div className='row'>
-          <div className='col-sm-4'>
-            <div className='panel panel-default'>
-              <div className='panel-heading'>Add New ToDo</div>
-              <div className='panel-body'>
-                <p>I want to..</p>
-                <input name='amountDue'/>
-              </div>
-              <div className='panel-body'>
-                <p>How much of a priority is this?</p>
-                <select className='priority'>
-                  <option>Low Priority</option>
-                  <option>Medium Priority</option>
-                  <option>High Priority</option>
-
-                </select>
-              </div>
-              <button className='btn btn-primary btn-block' name ='submit' type='button'>Add</button>
-            </div>
-          </div>
-
-          <div className='col-sm-8'>
-            <div className="panel panel-default"> 
-              <div className='panel-heading'>View ToDos</div>
-              <div className="alert alert-info" role="alert">
-                <h6>Welcome to Very Simple ToDo App!</h6>
-                <p>Get started now by adding a new todo on the left</p>
-              </div>
-            </div>
-          </div>
-          
-            
-
+          <AddTodo addTodoFn={this.addTodo}></AddTodo>
+          <TodoList></TodoList>
         </div>
       </div>
-                 
-               
     );
   }
 }
